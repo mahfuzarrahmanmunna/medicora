50316607c315f2ba31fa0be6c2cbf793090c3c89
mahfuzarrahmanmunna <mdmahfuzarrahmanmunna44@gmail.com>
2025-08-13 00:20:25 +0600

    added and edit medical services

--- app/components/MedicalServices/MedicalServices.jsx
+++ app/components/MedicalServices/MedicalServices.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 
 const MedicalServices = () => {
     return (
-        <div>
-            
+        <div className='bg-secondary px-4 sm:px-12 lg:p-24'>
+            <div>
+                <h5 className='text-primary'>MEDICAL SERVICES</h5>
+            </div>
         </div>
     );
 };
--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 import Banner from "./components/Banner/Banner";
 import CompleteCare from "./components/completeCate/CompleteCare";
+import MedicalServices from "./components/MedicalServices/MedicalServices";
 
 export default function Home() {
   return (
     <div className="space-y-24">
       <Banner />
       <CompleteCare />
+      <MedicalServices />
     </div>
   );
 }
